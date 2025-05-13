50c830c6903e2899c2e66127494a3f54b39c1ec7
rishikeshwagh17 <rishikesh.wagh1708@gmail.com>
2025-05-13 16:30:10 +0530

    basic api calls for the server crud operation api calls to test in postman

--- src/app.js
+++ src/app.js
@@ -2,6 +2,23 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ firstname: "John", lastname: "Doe" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("update the DB and return the user");
+  res.send("post api is done");
+});
+
+app.put("/updateUser", (req, res) => {
+  res.send("put api is done");
+});
+
+app.delete("/deleteUser", (req, res) => {
+  console.log("user deleted successfully");
+  res.send("delete api is done");
+});
 //handle the code for request
 app.use("/hello", (req, res) => {
   res.send("Hello from the server");
