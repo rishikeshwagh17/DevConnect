69250a6084921c09f3ffc05e7a6ccd2f6c117042
rishikeshwagh17 <rishikesh.wagh1708@gmail.com>
2025-05-20 16:02:47 +0530

    api for get one user and get all users

--- src/app.js
+++ src/app.js
@@ -7,13 +7,33 @@ const app = express();
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  console.log(req.body);
-
   //create a instance of the model
   const user = new UserModel(req.body);
   //save the data into the database
   await user.save();
   res.status(200).send("user added successfully");
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const user = await UserModel.find({ emailId: req.body.emailId });
+    if (user.length === 0) {
+      return res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong!");
+  }
 });
 
 connectDB()
